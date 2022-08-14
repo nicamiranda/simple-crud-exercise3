1abe70c073b0e130472727282c3551e0006a51c1
nicamiranda <gennicamiranda@gmail.com>
2022-08-14 13:29:52 +0800

    added exercise 2

--- src/app/blog/pages/blog-list/blog-list.component.ts
+++ src/app/blog/pages/blog-list/blog-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogService } from '../../service/blog.service';
+import { Blog } from '../../models/blog';
 
 @Component({
   selector: 'app-blog-list',
   templateUrl: './blog-list.component.html',
   styleUrls: ['./blog-list.component.scss']
 })
-export class BlogListComponent implements OnInit {
+export class BlogListComponent implements OnInit { 
+  blogs: Blog[] = [];
+  columnsToDisplay = ['id', 'title'];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private blogService: BlogService ) {
+    this.blogs = this.blogService.getBlogDetails();
+   }
+  
+  ngOnInit() : void {
+    
   }
 
 }
